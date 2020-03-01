e610cfab38c6106c59cfd01efcbbb63910afd98c
soju6jan <cybersol@naver.com>
2020-03-01 23:37:49 +0900

    release

---
+++ logic_normal.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+#########################################################
+# python
+import os
+import sys
+import traceback
+import time
+from datetime import datetime, timedelta
+import logging
+import urllib
+import urllib2
+import re
+import json
+
+# third-party
+import requests
+from flask import Blueprint, request, Response, send_file, render_template, redirect, jsonify
+from sqlalchemy import desc
+from lxml import html
+
+# sjva 공용
+from framework import db, scheduler
+from framework.job import Job
+from framework.util import Util
+from system import SystemLogicTrans
+import framework.common.fileprocess as FileProcess
+
+# 패키지
+from .plugin import logger, package_name
+from .model import ModelSetting
+#########################################################
+
+class LogicNormal(object):
+
+
+    @staticmethod
+    def proxy_init():
+        try:
+            if ModelSetting.get_bool('use_proxy'):
+                tmp = ModelSetting.get('proxy_url')
+                FileProcess.proxyes = { 
+                    "http"  : tmp, 
+                    "https" : tmp, 
+                }
+            else:
+                FileProcess.proxyes = None
+        except Exception as e: 
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+
+
+    ########################################################
+    
+    
+
+
+    @staticmethod
+    def test(req):
+        try:
+            target = req.form['target']
+            keyword = req.form['test']
+            if target == 'dmm':
+                return LogicNormal.test_dmm(keyword)
+            elif target == 'javdb':
+                return LogicNormal.test_javdb(keyword)
+            elif target == 'avgle':
+                return LogicNormal.test_avgle(keyword)
+        except Exception as e: 
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+
+
+    @staticmethod
+    def test_dmm(keyword):
+        try:
+            ret = {}
+            ret['search'] = FileProcess.dmm_search(keyword)
+            if len(ret['search']) == 1:
+                ret['update'] = FileProcess.dmm_update(ret['search'][0]['id'])
+            else:
+                for tmp in ret['search']:
+                    if tmp['score'] == 100:
+                        ret['update'] = FileProcess.dmm_update(tmp['id'])
+                        break
+            return ret
+        except Exception as e: 
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+
+
+    @staticmethod
+    def test_javdb(keyword):
+        try:
+            ret = {}
+            ret['search'] = FileProcess.javdb_search(keyword)
+            if len(ret['search']) == 1:
+                ret['update'] = FileProcess.javdb_update(ret['search'][0]['id'])
+            else:
+                for tmp in ret['search']:
+                    if tmp['score'] == 100:
+                        ret['update'] = FileProcess.javdb_update(tmp['id'])
+                        break
+            return ret
+        except Exception as e: 
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+            return False
+
+    
+    
+    @staticmethod
+    def test_avgle(keyword):
+        try:
+            ret = {}
+            ret['search'] = LogicNormal.avgle_search(keyword)
+            
+            if len(ret['search']['response']['videos']) >= 1:
+                ret['update'] = LogicNormal.avgle_update(ret['search']['response']['videos'][0]['vid'])
+            return ret
+        except Exception as e: 
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+            return False
+
+    @staticmethod
+    def avgle_search(keyword):
+        try:
+            session = requests.Session()
+            url = 'https://api.avgle.com/v1/jav/%s/0?limit=10' % keyword
+            response = session.get(url, headers=LogicNormal.headers, proxies=LogicNormal.proxyes)
+            data = response.json()
+            return data
+        except Exception as e: 
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+            return False
+    
+    @staticmethod
+    def avgle_update(vid):
+        try:
+            session = requests.Session()
+            url = 'https://api.avgle.com/v1/video/%s' % vid
+            response = session.get(url, headers=LogicNormal.headers, proxies=LogicNormal.proxyes)
+            data = response.json()
+            return data
+        except Exception as e: 
+            logger.error('Exception:%s', e)
+            logger.error(traceback.format_exc())
+            return False
+
+
+    ########################################################
+    
---
+++ plugin.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+#########################################################
+# python
+import os
+import traceback
+import time
+from datetime import datetime
+import urllib
+import json
+# third-party
+import requests
+from flask import Blueprint, request, Response, send_file, render_template, redirect, jsonify, session, send_from_directory 
+from flask_socketio import SocketIO, emit, send
+from flask_login import login_user, logout_user, current_user, login_required
+
+# sjva 공용
+from framework.logger import get_logger
+from framework import app, db, scheduler, path_data, socketio, check_api
+from framework.util import Util, AlchemyEncoder
+from system.logic import SystemLogic
+import framework.common.fileprocess as FileProcess
+
+# 로그
+package_name = __name__.split('.')[0]
+logger = get_logger(package_name)
+
+# 패키지
+from .model import ModelSetting
+from .logic import Logic
+from .logic_normal import LogicNormal
+
+
+#########################################################
+
+
+#########################################################
+# 플러그인 공용                                       
+#########################################################
+blueprint = Blueprint(package_name, package_name, url_prefix='/%s' %  package_name, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+menu = {
+    'main' : [package_name, 'AV Agent'],
+    'sub' : [
+        ['setting', '설정'], ['log', '로그']
+    ]
+}
+
+plugin_info = {
+    'version' : '0.1.0.0',
+    'name' : 'av_agent',
+    'category_name' : 'plex',
+    'developer' : 'soju6jan',
+    'description' : 'Plex SJVA AV Agent와 연동하는 플러그인',
+    'home' : 'https://github.com/soju6jan/av_agent',
+    'more' : '',
+}
+
+
+def plugin_load():
+    Logic.plugin_load()
+
+def plugin_unload():
+    Logic.plugin_unload()
+
+
+
+#########################################################
+# WEB Menu 
+#########################################################
+@blueprint.route('/')
+def home():
+    return redirect('/%s/setting' % package_name)
+
+@blueprint.route('/<sub>')
+@login_required
+def first_menu(sub): 
+    if sub == 'setting':
+        arg = ModelSetting.to_dict()
+        arg['package_name'] = package_name
+        arg['agent_server'] = SystemLogic.get_setting_value('ddns') + '/%s/api' % package_name
+        return render_template('%s_setting.html' % package_name, sub=sub, arg=arg)
+    elif sub == 'log':
+        return render_template('log.html', package=package_name)
+    return render_template('sample.html', title='%s - %s' % (package_name, sub))
+
+#########################################################
+# For UI                                                            
+#########################################################
+@blueprint.route('/ajax/<sub>', methods=['GET', 'POST'])
+@login_required
+def ajax(sub):
+    # 설정 저장
+    try:
+        if sub == 'setting_save':
+            ret = ModelSetting.setting_save(request)
+            LogicNormal.proxy_init()
+            return jsonify(ret)
+        elif sub == 'test':
+            ret = LogicNormal.test(request)
+            return jsonify(ret)
+    except Exception as e: 
+        logger.error('Exception:%s', e)
+        logger.error(traceback.format_exc())
+
+#########################################################
+# API - 외부
+#########################################################
+@blueprint.route('/api/<sub>', methods=['GET', 'POST'])
+@check_api
+def api(sub):
+    try:
+        if sub == 'search':
+            arg = request.args.get('code')
+            ret = FileProcess.search(arg)
+        elif sub == 'update':
+            arg = request.args.get('code')
+            ret = FileProcess.update(arg)
+        elif sub == 'image':
+            image_url = request.args.get('url')
+            logger.debug(image_url)
+            method = ModelSetting.get('javdb_landscape_poster')
+            if method == '0':
+                return redirect(image_url)
+
+            from PIL import Image
+            import requests
+            im = Image.open(requests.get(image_url, stream=True).raw)
+            width,height = im.size
+            logger.debug(width)
+            logger.debug(height)
+            if height > width * 1.5:
+                return redirect(image_url)
+            if method == '1':
+                if width > height:
+                    im = im.rotate(-90, expand=True)
+            elif method == '2':
+                if width > height:
+                    im = im.rotate(90, expand=True)
+            elif method == '3':
+                new_height = int(width * 1.5)
+                new_im = Image.new('RGB', (width, new_height))
+                new_im.paste(im, (0, int((new_height-height)/2)))
+                im = new_im
+
+            filename = os.path.join(path_data, 'tmp', 'rotate.jpg')
+            im.save(filename)
+            return send_file(filename, mimetype='image/jpeg')
+        return jsonify(ret)
+        
+    except Exception as e:
+        logger.debug('Exception:%s', e)
+        logger.debug(traceback.format_exc())
+
+
+"""
+@blueprint.route('/avgle/<mode>', methods=['GET', 'POST'])
+def avgle(mode):
+    try:
+        if mode == 'search':
+            try:
+                arg = request.args.get('arg')
+                ret = Logic.avgle_search(arg)
+                return jsonify(ret)
+            except Exception as e:
+                logger.debug('Exception:%s', e)
+                logger.debug(traceback.format_exc())  
+        elif mode == 'update':
+            try:
+                arg = request.args.get('arg')
+                ret = Logic.avgle_update(arg)
+                return jsonify(ret)
+            except Exception as e:
+                logger.debug('Exception:%s', e)
+                logger.debug(traceback.format_exc())  
+        
+    except Exception as e:
+        logger.debug('Exception:%s', e)
+        logger.debug(traceback.format_exc())
+        return 'fail'
+"""

58c71daf5df9dadba14ac12ce62c87bf0c37d040
soju6jan <cybersol@naver.com>
2020-03-04 19:03:19 +0900

    1

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from plugin import blueprint, menu, plugin_load, plugin_unload
+from plugin import blueprint, menu, plugin_load, plugin_unload, plugin_info
 from logic import Logic
 from logic_normal import LogicNormal
